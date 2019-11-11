b3c0e3148193bb7d60af8de378073f801ac703cb
Dariia Zaruba <zaruba.daria@spd-ukraine.com>
2019-11-11 19:54:06 +0200

    Commit 1

--- src/test/java/OrderAnProduct.java
+++ src/test/java/OrderAnProduct.java
@@ -50,36 +50,91 @@ public class OrderAnProduct {
 
 //        Qty +
         driver.findElement(By.id("cart_quantity_up_2_7_0_0")).click();
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
-//        Assets
+        driver.get("http://automationpractice.com/index.php?controller=order");
+        driver.navigate().refresh();
+
+//        Assets Total for Product
+        WebElement totalForProduct = driver.findElement(By.id("total_product_price_2_7_0"));
+        String actualTotalForProduct = totalForProduct.getText();
+        System.out.println("actualTotalForProduct: " + actualTotalForProduct);
+
+        String expectedTotalForProduct = "$54.00";
+        System.out.println("expectedTotalForProduct: " + expectedTotalForProduct);
+
+        Assert.assertEquals(actualTotalForProduct, expectedTotalForProduct);
+
 //        Assets Total products
-//        WebElement totalProducts = driver.findElement(By.xpath("//*[@id=\'total_product\']"));
-//        WebElement totalProducts1 = wait.until(ExpectedConditions.refreshed(totalProducts.));
         WebElement totalProducts = driver.findElement(By.xpath("//*[@id=\'total_product\']"));
         String actualTotalResults = totalProducts.getText();
         System.out.println("actualTotalResults: " + actualTotalResults);
 
         String expectedTotalResults = "$54.00";
         System.out.println("expectedTotalResults:" + expectedTotalResults);
+
         Assert.assertEquals(actualTotalResults,expectedTotalResults);
 
+//        Assets Total Shipping
+        WebElement totalShipping = driver.findElement(By.id("total_shipping"));
+        String actualTotalShipping = totalShipping.getText();
+        System.out.println("actualTotalShipping: " + actualTotalShipping);
+
+        String expectedTotalShipping = "$2.00";
+        System.out.println("expectedTotalShipping:" + expectedTotalShipping);
+
+        Assert.assertEquals(actualTotalShipping, expectedTotalShipping);
+
+//        Assets Total Price without Tax
+        WebElement totalWithoutTax = driver.findElement(By.id("total_price_without_tax"));
+        String actualTotalWithoutPrice = totalWithoutTax.getText();
+        System.out.println("actualTotalWithoutPrice: " + actualTotalWithoutPrice);
+
+        String expectedTotalWithoutPrice = "$56.00";
+        System.out.println("expectedTotalWithoutPrice: " + expectedTotalWithoutPrice);
+
+        Assert.assertEquals(actualTotalWithoutPrice, expectedTotalWithoutPrice);
+
+//        Assets Tax
+        WebElement tax = driver.findElement(By.id("total_tax"));
+        String actualTax = tax.getText();
+        System.out.println("actualTax: " + actualTax);
+
+        String expectedTax = "$0.00";
+        System.out.println("expectedTax: " + expectedTax);
+
+        Assert.assertEquals(actualTax, expectedTax);
+
+//        Assets TOTAL
+        WebElement total = driver.findElement(By.id("total_price"));
+        String actualTotal = total.getText();
+        System.out.println("actualTotal: " + actualTotal);
+
+        String expectedTotal = "$56.00";
+        System.out.println("expectedTotal: " + expectedTotal);
+
+        Assert.assertEquals(actualTotal, expectedTotal);
+
+//        Remove products
+        driver.findElement(By.id("2_7_0_0")).click();
+
+//        Asset for Empty Cart
+        WebElement emptyCart = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"center_column\"]/p")));
+
+        String actualEmptyCart = emptyCart.getText();
+        System.out.println("actual text on the page: " + actualEmptyCart);
+
+        String expectedEmptyCart = "Your shopping cart is empty.";
+        System.out.println("expected text on the page: " + expectedEmptyCart);
+
+        Assert.assertEquals(actualEmptyCart, expectedEmptyCart);
 
 
 
-
-
-
-
-
-//        //        6. Проверить что значения Total для товара , Total products,
-////        Total shipping , Total всех товаров , Tax и TOTAL общий отображается корректно
-
-//        WebElement alertBlock = driver.findElement(By.cssSelector(".alert.alert-danger"));
-//
-//        String expectedResult = "There are 3 errors";
-//        String actualResult = alertBlock.getText();
-//        System.out.println(actualResult);
-//        Assert.assertTrue(actualResult.contains(expectedResult));
 
 
 
